60a503bfceaf6ca03df297941cebf67f906186f5
Ravi <ravi@prodifyllp.com>
2025-07-03 18:30:43 +0530

    Login page UI implementation

---
+++ smart-appointment-booking-system/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'smart-appointment-booking-system' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('smart-appointment-booking-system');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, smart-appointment-booking-system');
+  });
+});
---
+++ smart-appointment-booking-system/src/app/components/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm !: FormGroup;
+
+  constructor(){
+    this.loginForm = new FormGroup({
+      email : new FormControl(),
+      password : new FormControl()
+    })
+  }
+}
